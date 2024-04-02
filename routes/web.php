16d3cd7165c4f0bd7f4911535372a1b22b3d6880
Korotkikhjuli <Korotkikhjuli@gmail.com>
2024-04-02 17:13:19 +0500

    com1

--- routes/web.php
+++ routes/web.php
@@ -61,5 +61,3 @@ Route::middleware(['admin'])->group(function(){
     Route::post('/ban', [MainController::class, 'ban'])->name('user.ban');
     Route::post('/rasban', [MainController::class, 'rasban'])->name('user.rasban');
 });
-
-
